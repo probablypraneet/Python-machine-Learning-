4a948f6f1b091591153e3bc6f40849c46bc374d0
probablypraneet <vc77bca05@vedascollege.edu.np>
2026-03-20 01:36:14 +0545

    Add all DataScience 2026 files

---
+++ testthis.py
@@ -0,0 +1,6 @@
+def InputName(fname, lname):
+    return f'hello {fname} {lname}\n you are welcome here'
+
+first_name = input("enter your first name: ")
+last_name = input("enter your last name: ")
+print(InputName(first_name, last_name))
